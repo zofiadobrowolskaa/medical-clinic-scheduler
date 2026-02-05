b8f257af2e8befe4ed2bca1d31ed4278336bd786
=zofia dobrowolska <z.dobrowolska.993@studms.ug.edu.pl>
2026-02-05 18:46:49 +0100

    chore: initialize project with spring boot dependencies

---
+++ src/main/java/com/clinic/medical_clinic_scheduler/MedicalClinicSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.clinic.medical_clinic_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalClinicSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalClinicSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinic/medical_clinic_scheduler/MedicalClinicSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinic.medical_clinic_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
